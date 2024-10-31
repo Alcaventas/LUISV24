043a2f183098e14a76fc54d78b75d970933bbbea
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2024-10-30 22:06:58 -0600

    Actualizar _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -8,13 +8,13 @@ let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner } ) {
 global.key = ''
 
-global.canales = [canal1, canal2, canal3, canal4].getRandom()
-global.welgata = [tk, ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb, tk, ths, asistencia].getRandom()
-global.redesMenu = [canal1, canal2, canal3, canal4, soporteGB, grupo1, grupo2, grupo3, grupo4, grupo5, grupo6, md, ig, paypal, yt, asistencia, fb, tk].getRandom()
-global.accountsgb = [canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths, md, asistencia].getRandom()
+global.canales = [ig, fb, tk].getRandom()
+global.welgata = [tk, ig, ig, ig, yt, paypal, , ig, fb, tk, ths, asistencia].getRandom()
+global.redesMenu = [ig, paypal, yt, asistencia, fb, tk].getRandom()
+global.accountsgb = [ tk, ig, yt, paypal, fb, ths, md, asistencia].getRandom()
 
 global.canalIdGB = ["120363160031023229@newsletter", "120363169294281316@newsletter", "120363203805910750@newsletter", "120363302472386010@newsletter"]
-global.canalNombreGB = ["INFINITY-WA 💫", "GB - UPDATE 🐈", "Tips sobre GataBot 🤩", "NEW PROJECT: YartexBot-MD ✨"]
+global.canalNombreGB = ["@Alcashop.ff"]
 global.channelRD = await getRandomChannel()
 
 // Imágenes 
